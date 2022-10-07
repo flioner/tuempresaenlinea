a0f7db2c83d6cd4368cb654d9e684588efa3d79f
flioner <lionerrochae@gmail.com>
2022-10-07 12:17:51 -0500

    test 7

--- components/slider/slider.tsx
+++ components/slider/slider.tsx
@@ -1,217 +1,94 @@
-import React from "react";
-import { useKeenSlider } from "keen-slider/react";
-import "keen-slider/keen-slider.min.css";
-import s from "./slider.module.css";
-import cn from "clsx";
-import { Module} from "./sliderSubComponent";
- 
-const Slider = ({ images }) => {
-  const maxSlides = 5;
-  var slides = 1;
-  if (images.length <= maxSlides) {
-    slides = images.length;
-  } else {
-    slides = maxSlides;
-  }
-  const [sliderRef] = useKeenSlider({
-    initial: 1,
+import cn from 'clsx'
+import s from './slider.module.css'
+import { useKeenSlider } from 'keen-slider/react'
 
-    mode: "snap",
+import React, { useState } from 'react'
 
-    slides: { perView: slides, spacing: 25 },
-    loop: true,
-  });
+interface CarouselProps {
+  images: { id: number | string; src: string; path: string; alt?: string }[]
+  className?: string
+}
 
-  return (
-    <div className="keen-slider" ref={sliderRef}>
-      <div className={s.middle}>
-        {[...Array(images.length).keys()].map((idx) => {
-          return (
-            <div key={idx} className="keen-slider__slide">
-              <div className={s.slide}>
-                <img
-                  className={!images[idx][2] ? s.image : s.imageCrop}
-                  src={images[idx][0]}
-                />
-                <div className={s.text}> {images[idx][1]} </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
-
-const AutoplaySlider = ({ images }) => {
-  const maxSlides = 5;
-  var slides = 1;
-  if (images.length <= maxSlides) {
-    slides = images.length;
-  } else {
-    slides = maxSlides;
-  }
-  const [sliderRef] = useKeenSlider(
-    {
-      initial: 1,
-
-      mode: "snap",
-
-      slides: { perView: slides, spacing: 25 },
-      loop: true,
-    },
-    [
-      (slider) => {
-        let timeout;
-        let mouseOver = false;
-        function clearNextTimeout() {
-          clearTimeout(timeout);
-        }
-        function nextTimeout() {
-          clearTimeout(timeout);
-          if (mouseOver) return;
-          timeout = setTimeout(() => {
-            slider.next();
-          }, 10000);
-        }
-        slider.on("created", () => {
-          slider.container.addEventListener("mouseover", () => {
-            mouseOver = true;
-            clearNextTimeout();
-          });
-          slider.container.addEventListener("mouseout", () => {
-            mouseOver = false;
-            nextTimeout();
-          });
-          nextTimeout();
-        });
-        slider.on("dragStarted", clearNextTimeout);
-        slider.on("animationEnded", nextTimeout);
-        slider.on("updated", nextTimeout);
-      },
-    ]
-  );
-
-  return (
-    <div className="keen-slider" ref={sliderRef}>
-      {[...Array(images.length).keys()].map((idx) => {
-        return (
-          <div key={idx} className={s.middle}>
-            <div className="keen-slider__slide">
-              <div className={s.slide}>
-                <img
-                  className={!images[idx][2] ? s.image : s.imageCrop}
-                  src={images[idx][0]}
-                />
-                <div className={s.text}> {images[idx][1]} </div>
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
-
-const VerticalSlider = ({ content }) => {
-  const [sliderRef] = useKeenSlider(
+const Slider: React.FC<CarouselProps> = ({ images }) => {
+  const [currentSlide, setCurrentSlide] = React.useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>(
     {
       initial: 0,
-
-      breakpoints: {
-        "(min-width: 1100px)": {
-          dragSpeed: 1,
-        },
+      slideChanged(slider) {
+        setCurrentSlide(slider.track.details.rel)
       },
-      dragSpeed: 0.5,
-      mode: "snap",
-      slides: { origin: "center", perView: 1.5, spacing: 10 },
+      created() {
+        setLoaded(true)
+      },
       loop: true,
-      vertical: true,
-      renderMode: "precision",
+      mode: 'snap',
+     
+      slides: { origin: 'center', perView: 3, spacing: 10 },
     },
     [
       (slider) => {
-        let timeout;
-        let mouseOver = false;
+        let timeout: ReturnType<typeof setTimeout>
+        let mouseOver = false
+
         function clearNextTimeout() {
-          clearTimeout(timeout);
+          clearTimeout(timeout)
         }
+
         function nextTimeout() {
-          clearTimeout(timeout);
-          if (mouseOver) return;
+          clearTimeout(timeout)
+          if (mouseOver) return
           timeout = setTimeout(() => {
-            slider.next();
-          }, 10000);
+            slider.next()
+          }, 5000)
         }
-        slider.on("created", () => {
-          slider.container.addEventListener("mouseover", () => {
-            mouseOver = true;
-            clearNextTimeout();
-          });
-          slider.container.addEventListener("mouseout", () => {
-            mouseOver = false;
-            nextTimeout();
-          });
-          nextTimeout();
-        });
-        slider.on("dragStarted", clearNextTimeout);
-        slider.on("animationEnded", nextTimeout);
-        slider.on("updated", nextTimeout);
+
+        slider.on('created', () => {
+          slider.container.addEventListener('mouseover', () => {
+            mouseOver = true
+            clearNextTimeout()
+          })
+          slider.container.addEventListener('mouseout', () => {
+            mouseOver = false
+            nextTimeout()
+          })
+          nextTimeout()
+        })
+
+        slider.on('dragStarted', clearNextTimeout)
+        slider.on('animationEnded', nextTimeout)
+        slider.on('updated', nextTimeout)
       },
     ]
-  );
+  )
 
   return (
-    <div className={cn("keen-slider", s.verticalCont)} ref={sliderRef}>
-      {[...Array(content.length).keys()].map((idx) => {
-        return (
-          <div key={idx} className="keen-slider__slide">
-            <div className={s.verticalSlide}>
-              <img className={s.verticalImg} src={content[idx][0]} />
-
-              <div className={s.verticalTextCont}>
-                <div className={s.verticalTitle}>{content[idx][1]}</div>
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
-
-
-const VerticalTest = ({ content }) => {
-  const [sliderRef] = useKeenSlider({
-    initial: 1,
-
-    mode: "snap",
-
-    slides: { origin: "center", perView: 2.5, spacing: 10 },
-    loop: true,
-  });
-
-  return (
-    <div className="keen-slider" ref={sliderRef}>
-      <div className={s.middle}>
-        {[...Array(content.length).keys()].map((idx) => {
-          return (
-            <div key={idx} className="keen-slider__slide">
-              <div className={s.slide}>
+    <>
+    <div
+        ref={sliderRef}
+        className={cn('keen-slider', s.root, s.navigationWrapper)}
+      >
+        {images.map(({ id, src, path, alt = 'Product Image' }, idx) => (
+          <div className={cn('keen-slider__slide', s.slide)} key={id}>
+          
+          
                 <img
-                  className={s.image}
-                  src={content[idx][0]}
+                  src={src}
+                  alt={alt}
+                  width={978}
+                  height={550}
+          
+                  className={s.img}
+         
+             
                 />
-                <div className={s.text}> {content[idx][1]} </div>
-              </div>
-            </div>
-          );
-        })}
+      
+     
+          </div>
+        ))}
       </div>
-    </div>
-  );
-};
-  
+    </>
+  )
+}
 
-export { Slider, AutoplaySlider, VerticalSlider, VerticalTest };
+export { Slider }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,47 @@
 import Layout from "../components/layout/layout";
-import { Slider, VerticalSlider, VerticalTest } from "../components/slider/slider";
+import { Slider} from "../components/slider/slider";
 import s from "../styles/Home.module.css";
 import React from 'react'
 
 
 export default function Home() {
+  const images = [
+    {
+      id: 0,
+      src: 'https://golowled.sirv.com/Carrusel/light_0.jpg',
+      path: '/0',
+    },
+    {
+      id: 1,
+      src: 'https://golowled.sirv.com/Carrusel/light_1.jpg',
+      path: '/search/basicas',
+    },
+    {
+      id: 2,
+      src: 'https://golowled.sirv.com/Carrusel/light_2.jpg',
+      path: '/search/diseno-3d-y-arquitectura',
+    },
+    {
+      id: 3,
+      src: 'https://golowled.sirv.com/Carrusel/light_3.jpg',
+      path: '/search/gamers-alto-desempeno',
+    },
+    {
+      id: 4,
+      src: 'https://golowled.sirv.com/Carrusel/light_4.jpg',
+      path: '/search/oficina',
+    },
+  ]
+
   return (
     <Layout>
       <div className={s.middle}>
         <div className={s.crop} /* Profile Slider */>
-          <Slider
-            images={[
-              [
-                "https://eddencei.sirv.com/profile%20pic/FabianLioner.jpg",
-                "Fabian",
-                true,
-              ],
-              [
-                "https://eddencei.sirv.com/profile%20pic/FabianLioner.jpg",
-                "Fabian Lioner",
-                true,
-              ],
-              [
-                "https://eddencei.sirv.com/profile%20pic/FabianLioner.jpg",
-                "Fabian Lioner",
-                true,
-              ],
-          
-            ]}
-          />
+          <Slider images={images} />
         </div>
       </div>
 
-      <div className={s.verticalGlow} /* Vertical Slider */>
-        <VerticalTest
-          content={[
-            [
-              "https://eddencei.sirv.com/TuEmpresaEnLinea/bgImg.jpg",
-              "Landing Page",
-            ],
-            [
-              "https://eddencei.sirv.com/TuEmpresaEnLinea/bgImg.jpg",
-              "E-Commerce",
-            ],
-            [
-              "https://eddencei.sirv.com/TuEmpresaEnLinea/bgImg.jpg",
-              "Portafolio Digital",
-            ],
-            [
-              "https://eddencei.sirv.com/TuEmpresaEnLinea/bgImg.jpg",
-              "Showroom",
-            ],
-          ]}
-        />
-      </div>
+
     </Layout>
   );
 }
