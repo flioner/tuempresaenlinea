b652f317d966f315262ad4f5916c6b50ef4b17fb
flioner <lionerrochae@gmail.com>
2022-10-07 13:16:04 -0500

    IT FUCKING WORKS

--- components/slider/slider.tsx
+++ components/slider/slider.tsx
@@ -1,30 +1,31 @@
 import React from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import s from './slider.module.css';
 
-const Slider = ({ images }) => {
-
-
+const ProfileSlider = ({ images }) => {
   return (
     <Swiper
-      spaceBetween={50}
+  
+      spaceBetween={15}
       slidesPerView={3}
       loop={true}
     >
-
-{images.map(({ id, src, path, alt = 'Product Image' }, idx) => (
-  <SwiperSlide key={id}>  <img
-  src={src}
-  alt={alt}
-  width={978}
-  height={550}
-/></SwiperSlide>
-        ))}  
+      {images.map(({ id, src, nombre }, idx) => (
+        <SwiperSlide  className={s.slide}   key={id}>  
+            <img
+            className={s.imageCrop} 
+            src={src}
+            width={978}
+            height={550}
+            />
+            <div className={s.text}> {nombre}</div>
+        </SwiperSlide>))}  
     </Swiper>
   );
 };
 
-export { Slider };
+export { ProfileSlider };
 
 
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,44 @@
 import Layout from "../components/layout/layout";
-import { Slider } from "../components/slider/slider";
+import { ProfileSlider } from "../components/slider/slider";
 import s from "../styles/Home.module.css";
 import React from 'react'
 
 
 export default function Home() {
-  const images = [
-    {
-      id: 0,
-      src: 'https://golowled.sirv.com/Carrusel/light_0.jpg',
-      path: '/0',
-    },
-    {
-      id: 1,
-      src: 'https://golowled.sirv.com/Carrusel/light_1.jpg',
-      path: '/search/basicas',
-    },
-    {
-      id: 2,
-      src: 'https://golowled.sirv.com/Carrusel/light_2.jpg',
-      path: '/search/diseno-3d-y-arquitectura',
-    },
-    {
-      id: 3,
-      src: 'https://golowled.sirv.com/Carrusel/light_3.jpg',
-      path: '/search/gamers-alto-desempeno',
-    },
-    {
-      id: 4,
-      src: 'https://golowled.sirv.com/Carrusel/light_4.jpg',
-      path: '/search/oficina',
-    },
-  ]
-
+  
   return (
     <Layout> 
-          <Slider images ={images}/>
+      <div className={s.middle}>
+      <div className={s.crop}>
+      <ProfileSlider images ={[
+        {
+          id: 0,
+          src: 'https://eddencei.sirv.com/TuEmpresaEnLinea/pfp.webp',
+          nombre: 'Dev 1',
+        },
+        {
+          id: 1,
+          src: 'https://eddencei.sirv.com/TuEmpresaEnLinea/pfp.webp',
+          nombre: 'Dev 2'
+        },
+        {
+          id: 2,
+          src: 'https://eddencei.sirv.com/TuEmpresaEnLinea/pfp.webp',
+          nombre: 'Dev 3'
+        },
+        {
+          id: 3,
+          src: 'https://eddencei.sirv.com/TuEmpresaEnLinea/pfp.webp',
+          nombre: 'Dev 4'
+        },
+        {
+          id: 4,
+          src: 'https://eddencei.sirv.com/TuEmpresaEnLinea/pfp.webp',
+          nombre: 'Dev 5'
+        },
+      ]}/>
+      </div>
+      </div>
     </Layout>
   );
 }
