851bb4ed6fb86253dd4306e866013caee3d3a8fe
flioner <lionerrochae@gmail.com>
2023-03-01 09:32:54 -0600

    navbar & footer

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -1,26 +1,31 @@
 import React from "react";
+import { Contact } from "../ui/contact/contact";
 import s from "./navbar.module.css";
 
+const NavBar = ({}) => {
+  return (
+    <div className={s.navContainer}>
+      <div className={s.combo}>
+        <img
+          className={s.icon}
+          src="https://res.cloudinary.com/dgb1oqgt3/image/upload/v1677684494/Icons/teel_unbpj5.png"
+        />
+        <div className={s.mainTitle}> Tu Empresa en Línea</div>
+      </div>
+      <Contact>
+        <div className={s.contacto}> Contacto </div>
+      </Contact>
+    </div>
+  );
+};
 
-  const NavBar = ({  }) => {
-    return ( <div className={s.navContainer}> </div>
-  
-    );
-  };
-  
-  const Footer = ({  }) => {
-    return ( 
-        <div >
-            <div className={s.footerSpacer}></div>
-            <div className={s.footer}></div> 
-        </div>
-  
-    );
-  };
-  
- 
-  
-  export { NavBar, Footer };
+const Footer = ({}) => {
+  return (
+    <div>
+      <div className={s.footerSpacer}></div>
+      <div className={s.footer}> Tu Empresa en Línea © 2023</div>
+    </div>
+  );
+};
 
-  
-  
+export { NavBar, Footer };
