cd44ee588eb7bcc8f87d73e07851cc0474b0b8ef
flioner <lionerrochae@gmail.com>
2023-03-01 09:43:52 -0600

    icon

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({ children }) {
         <title>{siteTitle}</title>
         <link
           rel="icon"
-          href="https://eddencei.sirv.com/TuEmpresaEnLinea/BriefcaseIcon.png"
+          href="https://res.cloudinary.com/dgb1oqgt3/image/upload/v1677685387/Icons/teel_v5vp78.ico"
         />
       </Head>
 
